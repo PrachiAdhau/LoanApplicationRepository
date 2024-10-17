4b55f1d717be301422e97c8e9a73ac472220c468
PrachiAdhau <prachiadhau1997@gmail.com>
2024-10-18 00:26:04 +0530

    changes done

--- Customer/src/main/java/com/customer/exception/InvalidPinCodeException.java
+++ Customer/src/main/java/com/customer/exception/InvalidPinCodeException.java
@@ -1,11 +1,8 @@
 package com.customer.exception;
 
-<<<<<<< HEAD
+
 public class InvalidPinCodeException extends RuntimeException {
-=======
-public class InvalidPinCodeException extends RuntimeException{
-	
->>>>>>> branch 'main' of https://github.com/PrachiAdhau/LoanApplicationRepository.git
+
 	public InvalidPinCodeException(String msg)
 	{
 		super(msg);
